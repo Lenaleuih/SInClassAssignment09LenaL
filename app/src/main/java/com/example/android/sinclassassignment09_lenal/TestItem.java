109cbb3750a716149acfa0fd6d137dcfd1732eed
Lenaleuih <lenaleuih@163.com>
2019-04-19 17:20:11 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/sinclassassignment09_lenal/TestItem.java
@@ -0,0 +1,12 @@
+package com.example.android.sinclassassignment09_lenal;
+
+public class TestItem {
+    String question;
+    String answer;
+
+    public TestItem(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+}
+
